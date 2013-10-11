9c35b93a9553efa6e83dbc0b406627eca5139160
ldicocco <lu.dicocco@gmail.com>
2013-10-11 13:48:21 +0200

    Refactoring

--- samples/WebAPI/PublicWeb/Scripts/App/App.js
+++ samples/WebAPI/PublicWeb/Scripts/App/App.js
@@ -1,44 +1,42 @@
 ﻿angular.module('publicWeb', ['ngResource'])
-    .provider("rlangis", function () {
-        this.$get = function ($rootScope) {
-            var connection = $.hubConnection();
-            var rlangisHubProxy = connection.createRlangisHubProxy();
-            function ServerProxy(rlangisHubProxy, name) {
-                var _self = this;
-                this.hubProxy = rlangisHubProxy;
-                this.name = name;
-                this.isConnected = false;
+    .factory("rlangis", function ($rootScope) {
+        var connection = $.hubConnection();
+        var rlangisHubProxy = connection.createRlangisHubProxy();
+        function ServerProxy(rlangisHubProxy, name) {
+            var _self = this;
+            this.hubProxy = rlangisHubProxy;
+            this.name = name;
+            this.isConnected = false;
 
-                this.hubProxy.onRlangisConnected(function (name, connectionId) {
-                    $rootScope.$apply(function () { if (name === _self.name) { _self.isConnected = true; } });
-                });
+            this.hubProxy.onRlangisConnected(function (name, connectionId) {
+                $rootScope.$apply(function () { if (name === _self.name) { _self.isConnected = true; } });
+            });
 
-                this.hubProxy.onRlangisDisconnected(function (name, connectionId) {
-                    $rootScope.$apply(function () { if (name === _self.name) { _self.isConnected = false; } });
-                });
+            this.hubProxy.onRlangisDisconnected(function (name, connectionId) {
+                $rootScope.$apply(function () { if (name === _self.name) { _self.isConnected = false; } });
+            });
 
-                this.checkStatus = function () {
-                    this.hubProxy.invoke('_isActive', this.name).done(function (res) { $rootScope.$apply(function () { _self.isConnected = res; }); });
-                };
-            }
-            rlangisHubProxy.onRlangisConnected(function (name, connectionId) { $rootScope.$emit("serverStatus", name, true); });
-            rlangisHubProxy.onRlangisDisconnected(function (name, connectionId) { $rootScope.$emit("serverStatus", name, false); });
-            var onRlangisNameConnected = function (onConnected) {
-                var func = function (name, connectionId) { $rootScope.$apply(onConnected.apply(rlangisHubProxy, arguments)); };
-                rlangisHubProxy.onRlangisConnected(func);
+            this.checkStatus = function () {
+                this.hubProxy.invoke('_isActive', this.name).done(function (res) { $rootScope.$apply(function () { _self.isConnected = res; }); });
             };
-            var onRlangisNameDisconnected = function (onDisconnected) {
-                var func = function (name, connectionId) { $rootScope.$apply(onDisconnected.apply(rlangisHubProxy, arguments)); };
-                rlangisHubProxy.onRlangisDisconnected(func);
-            };
-            var getServerProxy = function (name) { return new ServerProxy(rlangisHubProxy, name); };
-            var start = function () { return connection.start(); };
-            return {
-                getServerProxy: getServerProxy,
-                start: start,
-                onRlangisNameConnected: onRlangisNameConnected,
-                onRlangisNameDisconnected: onRlangisNameDisconnected
-            };
+        }
+        rlangisHubProxy.onRlangisConnected(function (name, connectionId) { $rootScope.$emit("serverStatus", name, true); });
+        rlangisHubProxy.onRlangisDisconnected(function (name, connectionId) { $rootScope.$emit("serverStatus", name, false); });
+        var onRlangisNameConnected = function (onConnected) {
+            var func = function (name, connectionId) { $rootScope.$apply(onConnected.apply(rlangisHubProxy, arguments)); };
+            rlangisHubProxy.onRlangisConnected(func);
+        };
+        var onRlangisNameDisconnected = function (onDisconnected) {
+            var func = function (name, connectionId) { $rootScope.$apply(onDisconnected.apply(rlangisHubProxy, arguments)); };
+            rlangisHubProxy.onRlangisDisconnected(func);
+        };
+        var getServerProxy = function (name) { return new ServerProxy(rlangisHubProxy, name); };
+        var start = function () { return connection.start(); };
+        return {
+            getServerProxy: getServerProxy,
+            start: start,
+            onRlangisNameConnected: onRlangisNameConnected,
+            onRlangisNameDisconnected: onRlangisNameDisconnected
         };
     })
     .factory('Country', ['$resource', function ($resource) { return $resource('bridge/apiServer/api/countries/:Id', { Id: '@id' }); }])
